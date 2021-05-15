c11c63f5b99ecfd9f6f48367231039557628cd92
Carlos Padron <Carlos.Padron@albea-group.com>
2021-05-14 20:54:54 -0500

    Add project files.

---
+++ CS_Console_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_Console_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
